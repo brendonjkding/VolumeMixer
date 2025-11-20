977e6f78e5171f9473e672a9d8b9d7b3ba852285
Brendon <brendonjkdingnew@gmail.com>
2025-11-20 14:39:57 +0800

    minor clean-up

--- FrontBoard.h
+++ FrontBoard.h
@@ -1,9 +1,12 @@
-#import <FrontBoard/FBProcessManager.h>
-
 @interface FBProcessState : NSObject 
 @property (assign,nonatomic) long long taskState;
 @end
 
 @interface FBProcess : NSObject
 @property (nonatomic,copy,readonly) FBProcessState *state;
+@end
+
+@interface FBProcessManager : NSObject
++ (id)sharedInstance;
+- (FBProcess *)processForPID:(int)arg1;
 @end
